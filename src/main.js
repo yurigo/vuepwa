670b9e3118a2f6467885dea2ee08c7f8dfb4ce96
Yuri <ygonzoo@gmail.com>
2023-06-02 05:14:26 +0200

    fix Rollup failed to resolve import "vue-audio-visual"
    remove vue-audio-visual

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { AVPlugin } from 'vue-audio-visual'
 
-createApp(App).use(AVPlugin).mount('#app')
+createApp(App).mount('#app')
